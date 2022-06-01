e27fa04d8bb648f8779d4ad0d51a3b6acb54102c
Chris <chris96.villarreal@gmail.com>
2022-05-31 23:25:37 -0500

    Checo BT UART LED Program

--- main.c
+++ main.c
@@ -1,33 +1,13 @@
-#include <stdio.h>
-#include "constants.h"
+/*
+  PROGRAMA PARA PROBAR COMUNICACIÓN UART CON BT
 
-#define TO_BOOL(X) (X > 0?1:0)
+  Se recibe un caracter via BT.
+  En caso de que sea 'a', se enciende el LED en RB7
+*/
 
-// Alarm Packet example
+#define LED PORTB.RB7
 
-// Alarm macros
-#define GET_ALARM_INDEX(X) ((X >> 4) & 0x0F);
-#define GET_ALARM_ACTIVE_STATE(X) (X & 0x1);
-#define GET_ALARM_DISPENSER_0_STATE(X) (X & ALARM_DISPENSER_0_MASK)
-#define GET_ALARM_DISPENSER_1_STATE(X) (X & ALARM_DISPENSER_1_MASK)
-#define GET_ALARM_DISPENSER_2_STATE(X) (X & ALARM_DISPENSER_2_MASK)
-// TODO: Macros para extraer hora y minuto
-
-#define GET_ALARM_HOUR(X)  (((X & 0xF0) >> ) + (X))
-#define GET_ALARM_MINUTE(X) (((X & 0xF0) >>) + (X) + (X))
-
-// Helper macros
-#define LESS_THAN_OR(X, Y, Z) (X < Y ? X : Z)
-
-#define LED_0 PORTB.RB0
-#define LED_1 PORTB.RB1
-#define LED_2 PORTB.RB2
-#define LED_3 PORTB.RB3
-#define LED_4 PORTB.RB4
-#define LED_5 PORTB.RB5
-#define LED_6 PORTB.RB6
-
-
+char dato;
 
 void main() {
   char fakeAlarm[3];
@@ -38,19 +18,24 @@ void main() {
 
   UART1_Init(9600);
   UART1_Write_Text("Inicializado");
-  // Alarm read loop
-  while (TRUE) {
-    UART1_Write_Text("Main loop");
+  delay_ms(100);
+
+  while (1) {
     
     if (UART1_Data_Ready() == 1) {
-      UART1_Read_Text(inputBuffer, ";", ALARM_PACKET_LENGTH + 1);
+      dato = UART1_Read();
     }
 
-    if (strlen(inputBuffer) == 3) {
-      update_alarm(inputBuffer);
+    switch (dato) {
+    case 'a':
+      LED = 1;
+      break;
+    
+    default:
+      LED = 0;
+      break;
     }
 
-    // check_alarms();
-    delay_ms(250);
+    delay_ms(100);
   }
 }
